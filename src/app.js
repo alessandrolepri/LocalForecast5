1e0343514ea649cabafe946a50706fe6a671ae3a
Alessandro Lepri <alex.lepri84@gmail.com>
2020-05-13 13:34:40 +0100

    Up and running

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// import './style.scss';
+
+
+class App extends Component {
+    render() { 
+        return (
+            <div>Hello World</div>
+        );
+    }
+}
+ 
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
